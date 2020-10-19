4e8319564b53e1daaa400ba66384624846649af8
Deniz Ezgi Kurt <denizezgikurt@gmail.com>
2020-10-19 19:42:00 +0200

    imgs and initial js variables

---
+++ main.js
@@ -0,0 +1,7 @@
+const slider = document.querySelector(".slider");
+slider.querySelector(".slider-item");
+const navPrev = document.querySelector(".go-prev");
+const navNext = document.querySelector(".go-next");
+
+const startAutoplay = document.querySelector(".start-autoplay");
+const stopAutoplay = document.querySelector(".stop-autoplay");
